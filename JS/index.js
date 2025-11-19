80e41475309015849b71764c3c55332ceaf92a7a
HNSidiya04 <hlomlasidiya2@gmail.com>
2025-11-19 18:42:13 +0200

    adding JS

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,21 @@
- 
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+
+document.querySelectorAll('.gallery img').forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active');
+
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild);
+        }
+        const img = document.createElement('img');
+        img.src = image.src;
+        lightbox.appendChild(img);
+    });
+});
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return;
+    lightbox.classList.remove('active');
+});
